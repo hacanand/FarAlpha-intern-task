9e7f08b20e80aa58683f774be2e3f46e30e73234
anand <anandpromax[at]gmail[dot]com>
2024-11-20 22:11:49 +0530

    Update sayHello endpoint response message

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import express from "express";
 const app = express();
 
-app.get("/sayHello", (req, res) => {
-  //send json response
-  res.json({ message: "Hello User hi how are you" });
+app.get("/sayHello", (_, res) => {
+ 
+  res.json({
+    message: "Hello User ! Welcome to Test API" });
 });
 
-app.get("/api", (req, res) => {
-  //send json response
-  res.json({ message:  "Welcome to the API!" });
-});
+ 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 }
 )
 
-//port 80 is used for http requests so sudo is required to run the server on port 80 --> sudo node index.js to run the server
+ 
